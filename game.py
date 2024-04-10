766d01a327fad762a98cdbfcd626fd7b7677fee9
yuvalTrip <77538019+yuvalTrip@users.noreply.github.com>
2024-04-10 16:47:17 +0300

    Convert values to a dictionary in start_game func (game.py) to insert a Json .

--- game.py
+++ game.py
@@ -1,3 +1,6 @@
+import json
+
+
 def start_game(num_people, angle_sun,recognize_points,decrease_points,totaltime):
     # Your game logic goes here
     print("Starting game with {} people and angle of sun {}.".format(num_people, angle_sun))
@@ -5,4 +8,20 @@ def start_game(num_people, angle_sun,recognize_points,decrease_points,totaltime)
     print("For each minute will pass in the game, you will loose {} points".format(decrease_points))
     print("The game will lasts {} seconds.".format(totaltime))
 
-# If you have other functions or classes related to your game, you can define them here as well
+  # Convert values to a dictionary
+    game_data = {
+        "num_people": num_people,
+        "angle_sun": angle_sun,
+        "recognize_points": recognize_points,
+        "decrease_points": decrease_points,
+        "total_time": totaltime
+    }
+    #print(game_data)
+
+
+
+ # Convert dictionary to JSON string
+#     game_data_json = json.dumps(game_data)
+# # Pass JSON string to the game class or function
+#     game.start_game(game_data_json)
+# Ensure that your game.start_game function accepts a JSON string as an argument.
